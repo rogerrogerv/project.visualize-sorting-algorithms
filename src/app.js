2aeb4501e740b95017f5f1bcc2f5f8a7c69cc8d6
Roger V <60841571+rogerrogerv@users.noreply.github.com>
2020-04-15 09:04:34 +0900

    Add some more tweaks to the html and css and add images

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@
 */
 
 // How can we use require here if it's frontend? We can thank webpack.
-const Sort = require("./Sort");
+//const Sort = require("./Sort");
 
 // A link to our styles!
 require("./index.css");
 
-const sort = new Sort();
-sort.sort();
+const images = ['01.jpg','02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg'];
+const appImageSpace = document.querySelector(".app_container");
 
 function createCheesyTitle(slogan) {
   const container = document.createElement("h1");
@@ -19,8 +19,8 @@ function createCheesyTitle(slogan) {
   return container;
 }
 
-const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
-document.getElementById("title").appendChild(title);
+// const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
+// document.getElementById("title").appendChild(title);
 
 /*
     An simple example of how you can make your project a bit more
@@ -29,12 +29,27 @@ document.getElementById("title").appendChild(title);
     In our `index.html` page, we have a short form.
     Here is the code that talks to it.
   */
-function changeTitle(event) {
-  event.preventDefault();
+// function changeTitle(event) {
+//   event.preventDefault();
   // console.log('What is an event?', event);
-}
+//}
 
-const form = document.querySelector("form");
-document.addEventListener("DOMContentLoaded", () => {
-  form.onsubmit = changeTitle;
-});
+// const form = document.querySelector("form");
+// document.addEventListener("DOMContentLoaded", () => {
+//   form.onsubmit = changeTitle;
+// });
+
+const newDiv = () => document.createElement("div");
+
+const next = () => {
+  let counter = 0;
+  let pageEl = newDiv();
+  let imageEl = document.createElement("img");
+  imageEl.src = "./images/" + images[counter];
+  pageEl.append(imageEl);
+  appImageSpace.append(pageEl);
+  counter += 1;
+};
+
+let nextButton = document.getElementById("next-button");
+nextButton.addEventListener("click", next());
